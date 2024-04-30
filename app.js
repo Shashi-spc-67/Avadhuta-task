5804d2c27733cbf73fa1f341aed5b9f3205af2eb
Shashi-spc-67 <shashishashirekha96@gmail.com>
2024-04-30 20:44:45 +0530

    task added

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const encodeURL = (longURL) => {
+
+    const shortCode = generateShortCode(longURL);
+    const shortURL = `https://myntra.com/${shortCode}`;
+    const urlMap = {};
+    urlMap[shortCode] = longURL;
+    return shortURL;
+  };
+  const generateShortCode = (longURL) => {
+    const hash = hashFunction(longURL);
+    return hash.substring(0, 6); 
+  };
+  const hashFunction = (str) => {
+    let hash = 0;
+    if (str.length === 0) return hash;
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charCodeAt(i);
+      hash = (hash << 5) - hash + char;
+      hash = hash & hash; 
+    }
+    return hash.toString();
+  };
+
+  const longURL = "https://www.flipkart.com/very/long/url";
+  const shortURL = encodeURL(longURL);
+ 
+  console.log(`Original URL: ${longURL}`);
+  console.log(`Shortened URL: ${shortURL}`)
+  document.write(`Original URL: ${longURL}<br>`)
+  document.write(`Shortened URL: ${shortURL} <br>`)
